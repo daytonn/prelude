74c85a0e99b9eb16fe22b0807f4d370e1d42d206
Dayton Nolan <daytonn@gmail.com>
2016-09-13 16:42:42 -0500

    Some configuration

--- personal/personal.el
+++ personal/personal.el
@@ -32,30 +32,33 @@
       (global-unset-key "\C-z")
       (load-theme 'neopolitan t)
       (menu-bar-mode 1)))
+
 (disable-theme 'zenburn)
 
 (global-set-key (kbd "s-v") 'yank)
 (global-set-key (kbd "<s-return>") 'newline-below)
-(global-set-key (kbd "S-<s-return>") 'newline-above)
-(global-set-key (kbd "s-z") 'undo)
+(global-set-key (kbd "<M-s-kp-2>") 'split-window-right)
 (global-set-key (kbd "s-s") 'save-buffer)
-(global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
-(global-set-key (kbd "s-o") 'helm-find-files)
+(global-set-key (kbd "s-z") 'undo)
 (global-set-key (kbd "C-s-p") 'move-text-up)
-(global-set-key (kbd "C-s-n") 'move-text-down)
+(global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "S-<s-return>") 'newline-above)
+(global-set-key (kbd "s-o") 'helm-find-files)
+(global-set-key (kbd "C-\\") 'other-window)
 (global-set-key (kbd "s-d") 'duplicate-line)
-
-(set-default 'cursor-type '(bar . 2));;; bar cursor
+(global-set-key (kbd "C-s-n") 'move-text-down)
+(global-set-key (kbd "s-f") 'projectile-grep)
+(global-set-key (kbd "s-t") 'find-file)
 (blink-cursor-mode t)
 (set-cursor-color "white")
 
-(scroll-bar-mode -1);;; no scroll bars
+(set-default 'cursor-type '(hbar . 2));;; bar cursor
 (set-fringe-style '(6 . 0))
 
 (global-linum-mode 1)
 (global-hl-line-mode -1)
 (setq linum-format " %d ")
-
+(toggle-scroll-bar -1)
 (global-visual-line-mode t)
 (set-face-attribute 'default nil :height 150)
 ;;; personal.el ends here
